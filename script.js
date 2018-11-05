52cb791bf115838fb6ea2e3ccd61deb56b666dfd
DclMaxLenire <43648539+DclMaxLenire@users.noreply.github.com>
2018-11-05 09:16:08 +0100

    finalVersion

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
-//  // Limité le nombre de caractère //
-// var caracrt = 4;
-// // Récupérer le premier nombre choisis //
-// var chiffreSelection1 = document.getElementById("chiffre");
-// // Choisir l'opérateur //
-// var operateur = document.getElementById("option").value;
-// console.log(operateur);
-// // Récupérer le deuxième nombre choisis
-// var chiffreSelection2 = document.getElementById("chiffre2");
+        // // Récupérer le premier nombre choisis //
+var chiffreSelection1;
+var chiffreSelection2;
+var resultat;
 
-// Vérifier si les caractères sont des chiffres //
+
+        // Vérifier si les caractères sont des chiffres //
 function verificationNombre(event)
     {
-    // La zone à vérifier //
-    var source = document.getElementById("chiffre"); 
-    // La valeur de la zone a vérifier //
-    var value = source.value; 
-    // Si ce n'est pas un nombre //
-    if (isNaN(parseFloat(value)))  
-    {   // Si ce n'est pas un nombre le backgroundColor devient rouge //
+            // La zone à vérifier //
+    var source = document.getElementById("chiffre1"); 
+            // La valeur de la zone a vérifier //
+    var valeur = source.value; 
+            // Si ce n'est pas un nombre //
+    if (isNaN(parseFloat(valeur)))  
+    {       // Si ce n'est pas un nombre le backgroundColor devient rouge //
         document.getElementById("chiffreSelection").style.backgroundColor="red";
-        // Si c'est un nombre le backgroundColor devient ou redevient blanc //
+          // Si c'est un nombre le backgroundColor devient ou redevient blanc //
     } else {
-        document.getElementById("chiffreSelection").style.backgroundColor="white";
+        document.getElementById("chiffreSelection").style.backgroundColor="green";
     }}
 
+function verificationNombre2(event) {
+    var source2 = document.getElementById("chiffre2");
+    var valeur2 = source2.value;
+    if (isNaN(parseFloat(valeur2))) {
+        document.getElementById("chiffreSelection2").style.backgroundColor="red";
+    } else {
+        document.getElementById("chiffreSelection2").style.backgroundColor="green";
+    }
+}
 
-// Enregistrer le premier nombre saisie //
-// Choisir l'opérateur //
-// Enregistrer le deuxième nombre saisie //
 // Faire le calcule //
-// Donner le résultat //
+boutonCalcul.addEventListener("click",
+function addition() {
+if(document.forms["calculatrice"].elements["calculListe"].value === "addition") {
+    chiffreSelection1 = parseFloa(document.forms["calculatrice"].elements["champs1"].value);
+    chiffreSelection2 = parseFloat(document.forms["calculatrice"].elements["champs2"].value);
+    resultat = chiffreSelection1 + chiffreSelection2;
+    document.getElementById("resultat").innerHTML=(resultat);
+}});
 
+boutonCalcul.addEventListener("click",
+function multiplication() {
+    if(document.forms["calculatrice"].elements["calculListe"].value === "multiplication") {
+        chiffreSelection1 = parseFloat(document.forms["calculatrice"].elements["champs1"].value);
+        chiffreSelection2 = parseFloat(document.forms["calculatrice"].elements["champs2"].value);
+        resultat = chiffreSelection1 * chiffreSelection2;
+        document.getElementById("resultat").innerHTML=(resultat);
+    }
+});
 
+boutonCalcul.addEventListener("click",
+function soustraction() {
+    if(document.forms["calculatrice"].elements["calculListe"].value === "soustraction") {
+        chiffreSelection1 = parseFloat(document.forms["calculatrice"].elements["champs1"].value);
+        chiffreSelection2 = parseFloat(document.forms["calculatrice"].elements["champs2"].value);
+        resultat = chiffreSelection1 - chiffreSelection2;
+        document.getElementById("resultat").innerHTML=(resultat);
+    }
+});
 
-
-
+boutonCalcul.addEventListener("click",
+function division() {
+    if(document.forms["calculatrice"].elements["calculListe"].value === "division") {
+        chiffreSelection1 = parseFloat(document.forms["calculatrice"].elements["champs1"].value);
+        chiffreSelection2 = parseFloat(document.forms["calculatrice"].elements["champs2"].value);
+        resultat = chiffreSelection1 / chiffreSelection2;
+        document.getElementById("resultat").innerHTML=(resultat);
+    }
+});
+nouveauCalcul.addEventListener("click",
+    function reset() {
+        location.reload();
+    }
+)
